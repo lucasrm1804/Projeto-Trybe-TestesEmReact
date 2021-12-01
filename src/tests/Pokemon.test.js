b817324abba6f3a6786bc87b88685b9423da67ea
Lucas Monteiro <lucasribeirom@hotmail.com>
2021-12-01 19:48:08 -0300

    arruma lint

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import Pokemon from '../components/Pokemon';
 import renderWithRouter from './renderWithRouter';
 import pokemons from '../data';
